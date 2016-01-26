817c2c936ba60027ca2fb0ec06e4f30a286b207c
Ben Kohl <bkohl93@gmail.com>
2016-01-26 14:03:36 -0500

    Added test with 1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Tests
+//  Author: Ben Kohl
+//  Date: 1/26/16
+//
+
+#include <gtest/gtest.h>
+#include <iostream>
+
+std::string arabicToRoman(int n)
+{
+    return "I";
+}
+
+int main(int argc, char * argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(aToR, testWithOne)
+{
+    EXPECT_EQ("I", arabicToRoman(1));
+}
